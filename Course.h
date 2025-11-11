d1b07efdbcef73bc73fed32a971e5092e4d3dd15
Adam Yasser <adam.y.aboulenien@gmail.com>
2025-11-11 09:23:39 +0200

    Course.h

--- Course.h
+++ Course.h
@@ -4,7 +4,20 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+
+class Course{
+    private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+
+    public:
+    void addStudent(const Student& s);
+    void displayCourseInfo();
+
+};
 
 
 
